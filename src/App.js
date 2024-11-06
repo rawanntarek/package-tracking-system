5c792b626e43464c854c698a4b48e4ab028c94d2
rawanntarek <rawaaan245@gmail.com>
2024-11-06 16:14:03 +0200

    List Of Orders front end / type of user in registration

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Registration from './components/Registration';
+import ListOfOrders from './components/ListOfOrders';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        
+        <Route path="/ListOfOrders" element={<ListOfOrders />} />
         <Route path="/" element={<Registration />} />
       </Routes>
     </Router>
