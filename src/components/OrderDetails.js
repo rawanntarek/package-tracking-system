b85ad8abb409c4f8256920a08ea01f39afb18106
Samaselim1 <samaaahmed112@gmail.com>
2024-11-07 15:03:35 +0200

    update orderdetails

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -5,6 +5,7 @@ import "./OrderDetails.css";
 const OrderDetails = () => {
   const { id } = useParams();
   const [order, setOrder] = useState(null);
+  const [courier, setCourier] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -21,13 +22,30 @@ const OrderDetails = () => {
         }
         const data = await response.json();
         setOrder(data);
+        if (data.courierID) {
+          fetchCourierDetails(data.courierID);
+        }
       } catch (error) {
         console.error("Error:", error);
       }
     };
 
+
     fetchOrderDetails();
   }, [id]);
+
+  const fetchCourierDetails = async (courierID) => {
+    try {
+      const courierResponse = await fetch(`http://localhost:3000/getcourier?id=${courierID}`);
+      if (!courierResponse.ok) {
+        throw new Error("Failed to fetch courier details");
+      }
+      const courierData = await courierResponse.json();
+      setCourier(courierData);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   const cancelOrder = async () => {
     if (order.status !== "pending") {
@@ -63,16 +81,24 @@ const OrderDetails = () => {
     <div>
       <header><h1>Order Details</h1></header>
       <form>
-        <div>
+      <div>
           <p><b>Order ID:</b> {order.id}</p>
           <p><b>Pickup Location:</b> {order.pickupLocation}</p>
           <p><b>Drop Off Location:</b> {order.dropOffLocation}</p>
           <p><b>Package Details:</b> {order.packageDetails}</p>
           <p><b>Delivery Time:</b> {order.deliveryTime}</p>
           <p><b>Status:</b> {order.status}</p>
-          <p><b>Courier Info:</b> {order.courierID}</p>
-          <button type="button" onClick={cancelOrder}>Cancel Order</button>
+          
+          {courier ? (
+            <div>
+              <p><b>Courier Name:</b> {courier.name}</p>
+            </div>
+          ) : (
+            order.courierID && <p><b>Courier Info:</b> No courier details available</p>
+          )}
         </div>
+          <button type="button" onClick={cancelOrder}>Cancel Order</button>
+        
       </form>
     </div>
   );
